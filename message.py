72e2fd3e162ebfa0e4c948d0e22200594e8dca74
kindsenior <kunio.einstein@gmail.com>
2015-12-15 00:14:35 +0900

    create and parse all attachment files

--- message.py
+++ message.py
@@ -43,56 +43,74 @@ class MessageDataDict(dict):
         self.receiver = base64.urlsafe_b64decode(self.payload["parts"][0]["body"]["data"].encode("ASCII")).split("\n")[1][0:-4]
         self.receive_date = filter(lambda x: x["name"] == "Date", self.payload["headers"])[0]["value"]# 受信日
 
-        self.__estimate = None
-        self.__invoice = None
-        self.__bill = None
+        self.__attachments = [None,None,None]
+        self.__attachment_paths = [None,None,None]
 
-        self.__estimate_path = None
-
-    def get_attachment_data(self,basename):
-        print("MessageData.get_attachment_data()")
-        if self.__estimate is None:
+    def attachment_data(self,basename,idx):
+        print("MessageData.attachment_data(" + basename + " " + str(idx) + ")")
+        if self.__attachments[idx] is None:
             for attachment_part in self.attachment_parts:
                 if attachment_part["filename"].encode("utf-8") == basename + ".pdf":
                     return self.__service.users().messages().attachments().get(userId="me",messageId=self.id,id=attachment_part["body"]["attachmentId"]).execute()["data"]
             return false
-        return self.__estimate
+        return self.__attachments[idx]
 
-    def get_estimate_data(self):
-        return self.get_attachment_data("御見積書")
+    def estimate_data(self):
+        return self.attachment_data("御見積書",0)
         
-    def get_invoice_data(self):
-        return self.get_attachment_data("納品書")
+    def invoice_data(self):
+        return self.attachment_data("納品書",2)
 
-    def get_bill_data(self):
-        return self.get_attachment_data("御請求書")
-        
+    def bill_data(self):
+        return self.attachment_data("御請求書",1)
+
+    def attachment_date(self,create_func,path_func,):
+        create_func()
+        os.system("pdftotext -upw 160398 " + path_func().encode("utf-8"))# textファイルへ変換
+        file_text = open(path_func().replace(".pdf",".txt")).read()
+        date_list = re.split("\D",re.findall("発行日.*\n",file_text)[0])
+        while "" in date_list: date_list.remove("")
+        return date_list
+
+    def estimate_date(self):
+        return self.attachment_date(self.create_estimate,self.estimate_path)
+
+    def invoice_date(self):
+        return self.attachment_date(self.create_invoice,self.invoice_path)
+
     def set_values(self):
         print "set_values()"
         order_data_dict = dict()
         
-        self.create_estimate()                
-        os.system("pdftotext -upw 160398 " + self.estimate_path().encode("utf-8"))# textファイルへ変換        
-        file_text = open(self.estimate_path().replace(".pdf",".txt")).read()# listへ変換
-
-        date_list = re.split("\D",re.findall("発行日.*\n",file_text)[0])
-        while "" in date_list: date_list.remove("")
-        self["orderdate"] = reduce(lambda x,y: x + "/" + y,date_list)# from estimate
-        self["duedate"] = reduce(lambda x,y: x + "/" + y,date_list)# from invoice
+        self["orderdate"] = reduce(lambda x,y: x + "/" + y,self.estimate_date())# from estimate
+        self["duedate"] = reduce(lambda x,y: x + "/" + y,self.invoice_date())# from invoice
         self["person"]  = self.receiver
+        file_text = open(self.estimate_path().replace(".pdf",".txt")).read()
         self["price"] = re.findall("[,0-9]+\n",file_text)[4].replace("\n","")
 
-    def estimate_path(self):
-        if self.__estimate_path is None:
-            self.__estimate_path = os.path.join("/tmp", self.id + self.attachment_parts[0]["filename"])
-        return self.__estimate_path
+    def attachment_path(self,idx):
+        if self.__attachment_paths[idx] is None:
+            self.__attachment_paths[idx] = os.path.join("/tmp", self.id + self.attachment_parts[idx]["filename"])
+        return self.__attachment_paths[idx]
 
-    def create_estimate(self):
-        if not os.path.exists(self.estimate_path()):
-            file_data = base64.urlsafe_b64decode(self.get_estimate_data().encode('UTF-8'))
-            f = open(self.estimate_path(), 'w')
+    def estimate_path(self):
+        return self.attachment_path(0)
+
+    def invoice_path(self):
+        return self.attachment_path(2)
+
+    def create_attachment(self,path_func,data_func):
+        if not os.path.exists(path_func()):
+            file_data = base64.urlsafe_b64decode(data_func().encode('UTF-8'))
+            f = open(path_func(), 'w')
             f.write(file_data)
             f.close()
+
+    def create_estimate(self):
+        self.create_attachment(self.estimate_path,self.estimate_data)
+
+    def create_invoice(self):
+        self.create_attachment(self.invoice_path,self.invoice_data)
 
     def open_estimate(self):
         estimate_path = self.estimate_path()
