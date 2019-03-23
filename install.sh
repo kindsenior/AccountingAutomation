c9a264bc6ef552df878ab71b9c2626540ef25688
kindsenior <kunio.einstein@gmail.com>
2019-03-23 18:59:41 +0900

    update installation

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 sudo apt-get install python-gdata python-googleapi
+sudo apt install chromium-browser chromium-chromedriver
+
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib openpyxl
